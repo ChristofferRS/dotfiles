61b6f82087d5ab19d015e919abe177ad65cf7d51
Christoffer <christofferrs@protonmail.com>
2024-06-16 21:04:08 -0100

    Commented out unneeded

--- nvim/.config/nvim/lua/plugins/luasnip.lua
+++ nvim/.config/nvim/lua/plugins/luasnip.lua
@@ -2,15 +2,18 @@ return {
     "L3MON4D3/LuaSnip",
     config = function()
         local ls = require("luasnip")
-        local s = ls.snippet
-        local sn = ls.snippet_node
-        local isn = ls.indent_snippet_node
-        local t = ls.text_node
-        local i = ls.insert_node
-        local f = ls.function_node
-        local c = ls.choice_node
-        local d = ls.dynamic_node
-        local r = ls.restore_node
+        
+        -- Apparantly not neccesarry?
+        -- local s = ls.snippet
+        -- local sn = ls.snippet_node
+        -- local isn = ls.indent_snippet_node
+        -- local t = ls.text_node
+        -- local i = ls.insert_node
+        -- local f = ls.function_node
+        -- local c = ls.choice_node
+        -- local d = ls.dynamic_node
+        -- local r = ls.restore_node
+
         vim.keymap.set({ "i", "s" }, "<c-j>", function()
             if ls.expand_or_jumpable() then
                 ls.expand_or_jump()
